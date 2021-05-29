afb661fd58f2dcfeff1a9b66d2e6502dde6d7c60
mtomyis <mtomyis@gmail.com>
2021-05-29 15:04:03 +0700

    view add biaya

--- app/src/main/java/com/conceptdesign/re_claim/CreateActivity.kt
+++ app/src/main/java/com/conceptdesign/re_claim/CreateActivity.kt
@@ -1,11 +1,18 @@
 package com.conceptdesign.re_claim
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class CreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+    }
+
+    fun btn_tambahbiaya(view: View) {
+        intent = Intent(applicationContext, AddbiayaActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/conceptdesign/re_claim/MainActivity.kt
+++ app/src/main/java/com/conceptdesign/re_claim/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.conceptdesign.re_claim
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun btn_createnew(view: View) {
+        intent = Intent(applicationContext, CreateActivity::class.java)
+        startActivity(intent)
+    }
+
 }
